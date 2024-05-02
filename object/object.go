156328f631fd86ef3bd2182724e6f4bb360035f6
Vitor Rodrigues <vpaulo@users.noreply.github.com>
2024-05-02 22:17:26 +0100

    Error handing

--- object/object.go
+++ object/object.go
@@ -6,6 +6,7 @@ type ObjectType string
 
 const (
 	NULL_OBJ = "NULL"
+	ERROR_OBJ = "ERROR"
 
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
@@ -43,3 +44,10 @@ type ReturnValue struct {
 
 func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
 func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
+
+type Error struct {
+	Message string
+}
+
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
+func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
